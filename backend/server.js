938ae1ba7caa5884b254e84931b4a4e5428d9c2c
agathasena-design <agatha.sena@ba.estudante.senai.br>
2026-09-24 21:18:10 +0000

    nomezinho

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 const express =
 require("express")
 //O require("express") procura o express que foi instalado antes")
-const colors = require("cors");
+const cors = require("cors");
 //Quero utilizar o pacote cors
 const app = express ();
 //Criar nossa aplicação
 app.use(cors());
-//Use o corsnesta aplicação 
+//Use o cors nesta aplicação 
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json({
-    produto: "Kinder Bueno"
-    preco: "10,99"
+    produto: "Kinder Bueno",
+    preco: "10,99",
+    categoria: "Chocolate"
    });
 });
-app.listen(3000, ( => {
+app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
-}))
+});
